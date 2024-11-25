33e050542d0b6c60d3fe6da007a73c355a760e3f
shashank22603 <119421424+shashank22603@users.noreply.github.com>
2024-11-25 18:59:35 +0530

    Update move_turtle.py
    code is extended for multiple huskies

--- src/turtle_pentagon/src/move_turtle.py
+++ src/turtle_pentagon/src/move_turtle.py
@@ -2,30 +2,63 @@
 
 import rospy
 from geometry_msgs.msg import Twist
+import time
 
-def move_turtle():
-    rospy.init_node('move_turtle')
+class HuskyMover:
+    def __init__(self, husky_ns):
+        self.husky_ns = husky_ns
+        self.pub = rospy.Publisher(f'/{self.husky_ns}/husky_velocity_controller/cmd_vel', Twist, queue_size=10)
+        self.rate = rospy.Rate(10)  # 10 Hz
+        self.pattern_duration = 10  # Duration for each pattern in seconds
+        self.current_pattern = 0
 
-    # Get the namespace from the parameter server
-    husky_ns = rospy.get_param('~husky_ns', 'husky_1')  # Default to 'husky_1' if not set
+    def move_husky(self):
+        start_time = time.time()
+        
+        while not rospy.is_shutdown():
+            elapsed_time = time.time() - start_time
 
-    # Create a publisher that publishes to the velocity controller of the specified Husky robot
-    pub = rospy.Publisher(f'/{husky_ns}/husky_velocity_controller/cmd_vel', Twist, queue_size=10)
+            # Change patterns every pattern_duration seconds
+            if elapsed_time > self.pattern_duration:
+                self.current_pattern = (self.current_pattern + 1) % 2
+                start_time = time.time()  # Reset timer
+            
+            move_cmd = Twist()
 
-    rate = rospy.Rate(10)  # 10 Hz
-    vel_msg = Twist()
+            # Define patterns
+            if self.current_pattern == 0:
+                # Pattern 1: Square formation
+                if self.husky_ns == "husky_1":
+                    move_cmd.linear.x = 0.1  # Move forward
+                elif self.husky_ns == "husky_2":
+                    move_cmd.linear.x = 0.1
+                elif self.husky_ns == "husky_3":
+                    move_cmd.linear.x = 0.1
+                elif self.husky_ns == "husky_4":
+                    move_cmd.linear.x = 0.1
+            else:
+                # Pattern 2: Rhombus formation
+                if self.husky_ns == "husky_1":
+                    move_cmd.linear.x = 0.5  # Move forward
+                    move_cmd.angular.z = 0.3  # Turn slightly
+                elif self.husky_ns == "husky_2":
+                    move_cmd.linear.x = 0.5
+                    move_cmd.angular.z = -0.3  # Turn slightly
+                elif self.husky_ns == "husky_3":
+                    move_cmd.linear.x = 0.5
+                    move_cmd.angular.z = 0.3
+                elif self.husky_ns == "husky_4":
+                    move_cmd.linear.x = 0.5
+                    move_cmd.angular.z = -0.3
 
-    # Set desired motion parameters (example motion)
-    vel_msg.linear.x = 0.5
-    vel_msg.angular.z = 0.5
-
-    while not rospy.is_shutdown():
-        pub.publish(vel_msg)
-        rate.sleep()
+            self.pub.publish(move_cmd)
+            self.rate.sleep()
 
 if __name__ == '__main__':
     try:
-        move_turtle()
+        rospy.init_node('husky_mover', anonymous=True)
+        husky_ns = rospy.get_param("~husky_ns")  # Get the namespace parameter
+        mover = HuskyMover(husky_ns)
+        mover.move_husky()
     except rospy.ROSInterruptException:
         pass
-
